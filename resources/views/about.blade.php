cdaf2e1b72ab656dd4fa1425ec5dc02998c7ba64
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2024-07-09 23:00:10 +0700

    [UPDATE] Create Layout and Nav Components

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-</head>
-<body>
-    <h1>Halaman About</h1>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    Ini adalah halaman home about!
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/framework', function () {
-    return view('welcome');
+    return view('welcome', ['title' => 'Tech Used']);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['title' => 'About']);
 });
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home']);
 });
